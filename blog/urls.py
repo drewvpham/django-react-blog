f49ff81d1549e763a57a961c9e8c3499e15363e6
Drew Pham <drewvpham@gmail.com>
2020-10-13 23:41:34 -0500

    prep for deploy;

--- blog/urls.py
+++ blog/urls.py
@@ -19,4 +19,5 @@ urlpatterns = [
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += re_path(r'^(?:.*)/?$', TemplateView.as_view(template_name="index.html")),
+
+urlpatterns += re_path('', TemplateView.as_view(template_name="index.html")),
